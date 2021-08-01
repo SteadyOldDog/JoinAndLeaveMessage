6c4d7844f9181105aa9112f70f2617aba884a461
BlackWings <1303843340@qq.com>
2021-08-01 18:37:18 +0800

    added Config.kt

---
+++ src/main/kotlin/Configs/Config.kt
@@ -0,0 +1,10 @@
+package online.bikinibottom.Configs
+
+import net.mamoe.mirai.console.data.AutoSavePluginConfig
+
+object Config : AutoSavePluginConfig("Configs"){
+    val joinMessage : String ="欢迎新群主加入，@我可查看帮助"
+    val leaveMessage : String ="有人跑路了~"
+    val enabledBot:Long =839516647L
+    val enbleAt:Boolean = true
+}
--- src/main/kotlin/JoinAndLeaveMessage.kt
+++ src/main/kotlin/JoinAndLeaveMessage.kt
@@ -1,8 +1,14 @@
 package online.bikinibottom
 
+import net.mamoe.mirai.Bot
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.event.GlobalEventChannel
+import net.mamoe.mirai.event.events.MemberJoinEvent
+import net.mamoe.mirai.event.events.MemberLeaveEvent
+import net.mamoe.mirai.message.data.At
 import net.mamoe.mirai.utils.info
+import online.bikinibottom.Configs.Config
 
 object JoinAndLeaveMessage : KotlinPlugin(
     JvmPluginDescription(
@@ -15,6 +21,37 @@ object JoinAndLeaveMessage : KotlinPlugin(
     }
 ) {
     override fun onEnable() {
-        logger.info { "Plugin loaded" }
+        logger.info { "JoinAndLeaveMessage Plugin loaded" }
+        GlobalEventChannel.subscribeAlways<MemberJoinEvent> { event ->
+            //监听到群员已加入群后，在开启了的群里发送一条消息，消息内容为Config.joinMessage
+
+            Bot.instances.forEach {
+                //判断哪个机器人开启了本插件功能，负责执行欢迎
+                if (it.id== Config.enabledBot)
+                //判断是否开启at加入者，开启了则at ta并发送欢迎消息
+                    if (Config.enbleAt) {
+
+//                        val messageChain : MessageChain =
+                        event.group.sendMessage(At(event.member.id) +Config.joinMessage)
+                    }//否则直接发送欢迎消息
+                    else {
+                        event.group.sendMessage(Config.joinMessage)
+                    }
+            }
+
+        }
+        GlobalEventChannel.subscribeAlways<MemberLeaveEvent> { event ->
+            //监听到群员退出群后，在开启了功能的群里发送一条消息，消息内容为Config.leaveMessage
+            //判断群是否开启，
+            Bot.instances.forEach {
+                //判断哪个机器人开启了功能
+                if (it.id== Config.enabledBot) {
+                    event.group.sendMessage(Config.leaveMessage)
+                    val at = At(event.member.id)
+                    event.group.sendMessage(at+event.member.nick)
+                }
+            }
+
+        }
     }
 }
